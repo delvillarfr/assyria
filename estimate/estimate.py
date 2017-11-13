Corrected order of parameters, added full_variables case to contrast with gradient, and added non_directional functionality.

estimate/estimate.py
@@ -30,9 +30,6 @@ process = config.get('paths', 'process')
 
 
 
-
-
-
 class Estimate(object):
 
     def __init__(self, build_type, lat = (36, 42), lng = (27, 45)):
@@ -85,7 +82,7 @@ class Estimate(object):
         # Set coordinates in degrees (this should be done in process)
         for v in ['long_x', 'lat_y']:
             self.df_coordinates[v] = np.rad2deg(self.df_coordinates[v].values,
-                                               dtype='float128')
+                                                dtype='float64')
 
         # Known cities
         self.df_known = self.df_coordinates.loc[
@@ -97,14 +94,12 @@ class Estimate(object):
             self.df_coordinates['cert'] == 3,
             ['id', 'long_x', 'lat_y']
         ]
-        # Set dtype for coordinates
-        for df in [self.df_known, self.df_unknown]:
-            df[['long_x', 'lat_y']] = df[['long_x', 'lat_y']].astype(np.float64)
+        ## Set dtype for coordinates
+        #for df in [self.df_known, self.df_unknown]:
+        #    df[['long_x', 'lat_y']] = df[['long_x', 'lat_y']].astype(np.float64)
 
+        self.num_cities_known = len(self.df_known)
         self.num_cities_unknown = len(self.df_unknown)
-
-        # Get number of variables
-        self.num_vars = 1 + self.num_cities + 2*self.num_cities_unknown
 
         # Add known locations to trade data
         self.df_main = self.df_iticount[['id_j', 'id_i', 's_ij']]
@@ -123,10 +118,14 @@ class Estimate(object):
         def objective(varlist):
             ''' This is the formulation for autograd. '''
             return self.sqerr_sum(varlist)
-
         self.grad = grad(objective)
-    
-    
+
+        def objective_full_vars(varlist):
+            ''' This is the formulation for autograd. '''
+            return self.sqerr_sum(varlist, full_vars=True)
+        self.grad_full_vars = grad(objective_full_vars)
+
+
     def haversine_approx(self, coord_i, coord_j):
         '''
         coord_i, coord_j: np.array. 2 columns, len(iticount) rows. First column
@@ -152,7 +151,6 @@ class Estimate(object):
         #return factor_out * np.hypot( diff[:, 0], factor_in * diff[:, 1],
         #                             dtype=np.float64 )
         return factor_out * np.sqrt( np.sum(diff**2, axis=1) )
-
 
 
     def fetch_dist_dep(self, lat_guess, lng_guess):
@@ -186,7 +184,7 @@ class Estimate(object):
         return self.haversine_approx(main[['lat_y_i', 'long_x_i']].values,
                                      main[['lat_y_j', 'long_x_j']].values)
 
-    
+
     def tile_nodiag(self, A):
         '''
         A: np.array.
@@ -209,21 +207,24 @@ class Estimate(object):
         return mat_A[np.nonzero(mat_A)]
 
 
-    def get_coordinate_pairs(self, lat_guess, lng_guess):
+    def get_coordinate_pairs(self, lat_guess, lng_guess, full_vars=False):
         '''
+        full_vars: bool. If True, the known coordinates are included as
+        variables of the objective and gradient.
+
         This is an alternative implementation of the fetching distance process,
-        
+
         Leverages the fact that the iticount data is sorted according to
         id_jhwi_j first, then by id_jhwi_i, and the coordinates are sorted
         according to id_jhwi.
         '''
-        lats = np.concatenate((self.df_known['lat_y'].values, lat_guess))
-        longs = np.concatenate((self.df_known['long_x'].values, lng_guess))
+        if full_vars:
+            lats = lat_guess
+            longs = lng_guess
+        else:
+            lats = np.concatenate((self.df_known['lat_y'].values, lat_guess))
+            longs = np.concatenate((self.df_known['long_x'].values, lng_guess))
 
-        # Set lats and longs to max precision
-        #lats = lats.astype(np.float64)
-        #longs = longs.astype(np.float64)
-        
         coord_j = np.column_stack((
             np.repeat(lats, self.num_cities-1),
             np.repeat(longs, self.num_cities-1)
@@ -232,25 +233,26 @@ class Estimate(object):
             self.tile_nodiag(lats),
             self.tile_nodiag(longs)
         ))
+
         return (coord_i, coord_j)
 
 
-    def fetch_dist(self, lat_guess, lng_guess):
+    def fetch_dist(self, lat_guess, lng_guess, full_vars=False):
         '''
         Wrapper
         '''
-        coords = self.get_coordinate_pairs(lat_guess, lng_guess)
+        coords = self.get_coordinate_pairs(lat_guess, lng_guess, full_vars)
         return self.haversine_approx( coords[0], coords[1] )
 
-    
+
     def s_ij_model(self, zeta, alpha, distances):
         '''
         zeta: float. The distance elasticity of trade.
-        alpha: np.array. One for each importing city. 
+        alpha: np.array. One for each importing city.
         distances: np.array. Contains distances between all j, i pairs,
         excluding j, j pairs.
 
-        Idea: cast elements as matrix, add over axis=0, 
+        Idea: cast elements as matrix, add over axis=0,
         repeat (number of cities - 1), divide elements by this new 1-dim array.
 
         Returns np.array: the model-predicted trade shares
@@ -265,35 +267,54 @@ class Estimate(object):
         return elems / denom
 
 
-    def sqerr_sum(self, varlist):
+    def get_dividing_indices(self, full_vars=False):
         '''
-        varlist = [zeta, alpha, lat_guess, lng_guess].
+        Returns a dict with indices to extract latitudes and longitudes,
+        and alphas begin.
+        '''
+        if full_vars:
+            result = {'long_unknown_s': 2 + self.num_cities_known,
+                      'long_s': 2,
+                      'long_e': 2 + self.num_cities,
+                      'lat_unknown_s': 2 + self.num_cities + self.num_cities_known,
+                      'lat_s': 2 + self.num_cities,
+                      'lat_e': 2 + 2*self.num_cities,
+                      'a_s': 2 + 2*self.num_cities}
+        else:
+            result = {'long_s': 1,
+                      'long_e': 1 + self.num_cities_unknown,
+                      'lat_s': 1 + self.num_cities_unknown,
+                      'lat_e': 1 + 2*self.num_cities_unknown,
+                      'a_s': 1 + 2*self.num_cities_unknown}
+        return result
+
+
+    def sqerr_sum(self, varlist, full_vars=False):
+        '''
+        varlist = np.array([zeta, alpha, lat_guess, lng_guess]).
 
         Returns the value of the objective function given the data and the
         model trade shares.
         '''
         # Unpack arguments
         zeta = varlist[0]
-        alpha = varlist[1: 1 + self.num_cities]
-        lat_guess = varlist[1 + self.num_cities:
-                              1 + self.num_cities + self.num_cities_unknown]
-        lng_guess = varlist[1 + self.num_cities + self.num_cities_unknown: ]
+
+        i = self.get_dividing_indices(full_vars)
+        lng_guess = varlist[i['long_s']: i['long_e']]
+        lat_guess = varlist[i['lat_s']: i['lat_e']]
+        alpha = varlist[i['a_s']:]
+
         assert len(lat_guess) == len(lng_guess)
 
         s_ij_data = self.df_main['s_ij'].values
         s_ij_model = self.s_ij_model(zeta,
                                      alpha,
-                                     self.fetch_dist(lat_guess, lng_guess)
+                                     self.fetch_dist(lat_guess,
+                                                     lng_guess,
+                                                     full_vars)
                                     )
         diff = s_ij_data - s_ij_model
         return np.dot(diff, diff)
-
-
-    def grad_eval(self, varlist):
-        '''
-        Evaluates Gradient function.
-        '''
-        return self.grad(varlist)
 
 
     def replace_id_coord(self, constr, drop_wahsusana=False):
@@ -309,7 +330,7 @@ class Estimate(object):
         if drop_wahsusana:
             v = ['lb_lambda', 'ub_lambda', 'lb_varphi', 'ub_varphi']
             constr[v] = constr[v].replace('wa01', np.nan)
-            
+
         (ids, lats, lngs) = (self.df_coordinates['id'].values,
                               self.df_coordinates['lat_y'].values,
                               self.df_coordinates['long_x'].values
@@ -324,9 +345,8 @@ class Estimate(object):
                               )
             except TypeError:
                 constr[var] = constr[var].replace(np.nan, tracker[var])
-                print(constr[var])
 
-        
+
         tracker = {'lb_varphi': self.lng[0], 'ub_varphi': self.lng[1]}
         for var in tracker.keys():
             try:
@@ -338,40 +358,61 @@ class Estimate(object):
 
         return constr
 
-    
-    def get_bounds(self, constr):
+
+    def get_bounds(self, constr, full_vars=False):
         '''
         Returns (lb, ub), where lb and ub are lists for the bounds.
         '''
-        lb = self.num_vars * [-1.0e20]
-        ub = self.num_vars * [1.0e20]
-        
+        # Build specs: Ursu does not participate if directional, Kanes is in 
+        # second position in coordinates dataframe if directional, third
+        # otherwise.
+        if self.build_type == 'directional':
+            constr = constr[constr['id'] != 'ur01']
+            kanes_loc = 1
+        else:
+            kanes_loc = 2
+
+        if full_vars:
+            num_vars = 2 + 3*self.num_cities
+        else:
+            num_vars = 1 + 2*self.num_cities_unknown + self.num_cities
+
+        lb = num_vars * [-1.0e20]
+        ub = num_vars * [1.0e20]
+
         # zeta should be larger than zero
         lb[0] = 0.0
 
-        # alphas should be larger than zero
-        lb[1: 1 + self.num_cities] = self.num_cities * [0.0]
+
+        i = self.get_dividing_indices(full_vars)
+
+        dit = {'long':('varphi', 'long_x'), 'lat': ('lambda', 'lat_y')}
+        if full_vars:
+            lb[1] = 0
+            ub[1] = 0
+            for c in dit.keys():
+                # Known locations are given
+                lb[i[c+'_s']: i[c+'_unknown_s']] = self.df_known[dit[c][1]].tolist()
+                ub[i[c+'_s']: i[c+'_unknown_s']] = self.df_known[dit[c][1]].tolist()
+                # Unknown location constraints
+                lb[i[c+'_unknown_s']: i[c+'_e']] = constr['lb_'+dit[c][0]].tolist()
+                ub[i[c+'_unknown_s']: i[c+'_e']] = constr['ub_'+dit[c][0]].tolist()
+        else:
+            for c in dit.keys():
+                # Unknown location constraints
+                lb[i[c+'_s']: i[c+'_e']] = constr['lb_'+dit[c][0]].tolist()
+                ub[i[c+'_s']: i[c+'_e']] = constr['ub_'+dit[c][0]].tolist()
+
+        lb[i['a_s']:] = self.num_cities * [0.0]
 
         # Kanes' alphas are normalized to 100
-        # Note Kanes is in index 2 in coordinates dataframe.
-        lb[2] = 100.0
-        ub[2] = 100.0
-
-        # Unknown cities coordinates are bounded by the constraints.
-        # Note that Ursu does not participate.
-        constr = constr[constr['id'] != 'ur01']
-        lats_end_index = 1 + self.num_cities + self.num_cities_unknown
-        ## Latitudes
-        lb[1 + self.num_cities: lats_end_index] = constr['lb_lambda'].tolist()
-        ub[1 + self.num_cities: lats_end_index] = constr['ub_lambda'].tolist()
-        ## Longitudes
-        lb[lats_end_index:] = constr['lb_varphi'].tolist()
-        ub[lats_end_index:] = constr['ub_varphi'].tolist()
+        lb[i['a_s'] + kanes_loc] = 100.0
+        ub[i['a_s'] + kanes_loc] = 100.0
 
         return (lb, ub)
 
 
-    def initial_cond(self, len_sim=None, perturb=None):
+    def initial_cond(self, full_vars=False, len_sim=None, perturb=None):
         '''
         len_sim: int. Specifies the number of draws to take.
         perturb: float. Specifies a percentage deviation from the default
@@ -382,10 +423,16 @@ class Estimate(object):
         '''
         # Form default initial value
         zeta = [2.0]
-        alphas = self.num_cities * [1.0]
-        lats = self.df_unknown['lat_y'].tolist()
-        longs = self.df_unknown['long_x'].tolist()
-        x0 = zeta + alphas + lats + longs
+        alphas = np.ones(self.num_cities)
+        if full_vars:
+            # add tilde_delta0
+            zeta = zeta + [2.0]
+            lats = self.df_coordinates['lat_y'].values
+            longs = self.df_coordinates['long_x'].values
+        else:
+            lats = self.df_unknown['lat_y'].values
+            longs = self.df_unknown['long_x'].values
+        x0 = np.concatenate((zeta, longs, lats, alphas))
 
         # Perturb it
         if perturb != None:
@@ -393,7 +440,7 @@ class Estimate(object):
             p = np.random.uniform(1-perturb, 1+perturb, size=(len_sim, 1))
             x0 = x0*p
 
-        return x0
+        return np.array(x0)
 
 
     def solve(self, x0, constraint_type='static'):
@@ -412,16 +459,13 @@ class Estimate(object):
                              + "'static' or 'dynamic'")
         bounds = self.get_bounds(constr)
 
-        lb = bounds[0]
-        ub = bounds[1]
-
-        assert len(lb) == len(x0)
+        assert len(bounds[0]) == len(x0)
 
         nlp = ipopt.problem( n=len(x0),
                              m=0,
                              problem_obj=Optimizer(self.build_type),
-                             lb=lb,
-                             ub=ub )
+                             lb=bounds[0],
+                             ub=bounds[1] )
 
         # Add IPOPT options (some jhwi options were default)
         option_specs = { 'hessian_approximation': 'limited-memory',
@@ -437,7 +481,7 @@ class Estimate(object):
             nlp.addOption(option, option_specs[option])
 
         (x, info) = nlp.solve(x0)
-        
+
         # Set up variable names
         alphas = ['{0}_a'.format(i) for i in self.df_coordinates['id'].tolist()]
         lats = ['{0}_lat'.format(i) for i in self.df_unknown['id'].tolist()]
@@ -455,7 +499,7 @@ class Estimate(object):
 
     def gen_data(self, len_sim, perturb, rank=None):
         '''
-        rank: in parallelized computing.
+        rank: int. Process number in parallelized computing.
         Returns simulation dataframe sorted by objective value
         '''
         # Get initial values
@@ -465,25 +509,24 @@ class Estimate(object):
         for i in range(1, len_sim):
             i_val = x0[i, :].tolist()
             data = data.append( self.solve(i_val) )
-        
+
         if rank != None:
             data['process'] = rank
 
         # Sort
         return data.sort_values('obj_val')
 
+
+
+
 # Now define optimization problem
 class Optimizer(Estimate):
 
     def __init__(self, build_type):
         Estimate.__init__(self, build_type)
-        
+
     def objective(self, varlist):
         return -self.sqerr_sum(varlist)
 
     def gradient(self, varlist):
-        return -self.grad_eval(varlist)
-
-
-#e = Estimate('directional')
-#e.gen_data( 10000, 0.5 )
+        return -self.grad(varlist)

estimate/test_estimate.py
@@ -27,14 +27,21 @@ def test_iticount_has_all_possible_city_combinations():
                                   * (len(e.df_coordinates)-1))
 
 
-def test_same_known_and_unknown_cities():
+def test_same_known_and_unknown_cities_dir():
     ''' 15 (11) (un)known cities, acc to line 73 of main_script_directional.m '''
     e = estimate.Estimate('directional')
     assert len(e.df_unknown) == 11
     assert len(e.df_known) == 15
 
 
-def test_same_constraints_after_coord_replace_directional():
+def test_same_known_and_unknown_cities_nondir():
+    ''' Amkuwa (known) and Ursu (unknown) enter the non-directional analysis.'''
+    e = estimate.Estimate('non_directional')
+    assert len(e.df_unknown) == 12
+    assert len(e.df_known) == 16
+
+
+def test_same_constraints_after_coord_replace_directional_dir():
     e = estimate.Estimate('directional')
     replaced_dyn = e.replace_id_coord(e.df_constr_dyn)
     replaced_stat = e.replace_id_coord(e.df_constr_stat)
@@ -42,33 +49,33 @@ def test_same_constraints_after_coord_replace_directional():
 
 def test_same_distances_with_haversine_approx_and_euclidean_dist():
     e = estimate.Estimate('directional')
-    coords = pd.read_csv('test_distance_fn.csv')
+    coords = pd.read_csv('./matlab_tests/data/coords_sample.csv')
     dist_mine = e.haversine_approx(coords[['lati', 'longi']].values,
                                    coords[['latj', 'longj']].values)
 
-    dist_jhwi = pd.read_csv('distances_jhwi.csv')
+    dist_jhwi = pd.read_csv('./matlab_tests/data/distances_sample.csv')
 
-    np.testing.assert_array_equal(np.round(dist_jhwi.values, 6),
-                                  np.round(dist_mine.reshape((500,1)), 6))
+    np.testing.assert_array_almost_equal(dist_jhwi.values,
+                                         dist_mine.reshape((500,1)))
 
 
 def test_same_distances_with_haversine_approx_and_euclidean_dist_sq():
     e = estimate.Estimate('directional')
-    coords = pd.read_csv('test_distance_fn.csv')
+    coords = pd.read_csv('./matlab_tests/data/coords_sample.csv')
     dist_mine = (e.haversine_approx(coords[['lati', 'longi']].values,
                                     coords[['latj', 'longj']].values))**2
 
-    dist_jhwi = pd.read_csv('distances_sq_jhwi.csv')
+    dist_jhwi = pd.read_csv('./matlab_tests/data/distances_sq_sample.csv')
 
-    np.testing.assert_array_equal(np.round(dist_jhwi.values, 5),
-                                  np.round(dist_mine.reshape((500,1)), 5))
+    np.testing.assert_array_almost_equal(dist_jhwi.values,
+                                         dist_mine.reshape((500,1)))
 
 
-def test_coordinate_pairs_match_those_of_their_corresponding_iticount_entry():
+def test_coordinate_pairs_match_those_of_their_corresponding_iticount_entry_dir():
     e = estimate.Estimate('directional')
-    lats = pd.read_csv('lats_test.csv')
+    lats = pd.read_csv('./matlab_tests/data/lats_rand_dir.csv')
     lats = lats.iloc[0, :].values
-    longs = pd.read_csv('longs_test.csv')
+    longs = pd.read_csv('./matlab_tests/data/longs_rand_dir.csv')
     longs = longs.iloc[0, :].values
 
     e.df_unknown['lat_y'] = lats
@@ -76,7 +83,7 @@ def test_coordinate_pairs_match_those_of_their_corresponding_iticount_entry():
 
     coords = e.df_known.append(e.df_unknown)
     print(coords)
-    
+
     merged = e.df_iticount.copy()
     merged = merged.astype(np.float64, errors='ignore')
     coords_merged = []
@@ -88,70 +95,332 @@ def test_coordinate_pairs_match_those_of_their_corresponding_iticount_entry():
         merged = merged.rename(columns = {'long_x': 'long_x'+s,
                                           'lat_y': 'lat_y'+s})
         coords_merged.append(merged[['lat_y'+s, 'long_x'+s]].values)
-    
+
     coords_estimate = e.get_coordinate_pairs(lats, longs)
-    
+
     for i in range(2):
-        #np.testing.assert_array_equal(coords_estimate[i], coords_merged[i])    
         np.testing.assert_array_almost_equal(coords_estimate[i],
-                                             coords_merged[i])    
+                                             coords_merged[i])
 
 
+def test_coordinate_pairs_match_those_of_their_corresponding_iticount_entry_nondir():
+    e = estimate.Estimate('non_directional')
+    lats = pd.read_csv('./matlab_tests/data/lats_rand_nondir.csv')
+    lats = lats.iloc[0, :].values
+    longs = pd.read_csv('./matlab_tests/data/longs_rand_nondir.csv')
+    longs = longs.iloc[0, :].values
 
-def test_same_fetched_distances_directional():
+    e.df_unknown['lat_y'] = lats
+    e.df_unknown['long_x'] = longs
+
+    coords = e.df_known.append(e.df_unknown)
+    print(coords)
+
+    merged = e.df_iticount.copy()
+    merged = merged.astype(np.float64, errors='ignore')
+    coords_merged = []
+    for s in ['_i', '_j']:
+        merged = merged.merge(coords,
+                              how='left',
+                              left_on='id'+s,
+                              right_on='id')
+        merged = merged.rename(columns = {'long_x': 'long_x'+s,
+                                          'lat_y': 'lat_y'+s})
+        coords_merged.append(merged[['lat_y'+s, 'long_x'+s]].values)
+
+    coords_estimate = e.get_coordinate_pairs(lats, longs)
+
+    for i in range(2):
+        np.testing.assert_array_almost_equal(coords_estimate[i],
+                                             coords_merged[i])
+
+
+def test_same_fetched_distances_dir():
     e = estimate.Estimate('directional')
     zeta = 2
     #alpha has 26 entries, the num of cities.
     alpha = np.ones(26)
-    lats = pd.read_csv('lats_test.csv')
+    lats = pd.read_csv('./matlab_tests/data/lats_rand_dir.csv')
     lats = lats.iloc[0, :].values
-    longs = pd.read_csv('longs_test.csv')
+    longs = pd.read_csv('./matlab_tests/data/longs_rand_dir.csv')
     longs = longs.iloc[0, :].values
-    
+
     distances_mine = e.fetch_dist(lats, longs).reshape((650,1))
-    #i = e.df_iticount['id_i'].values
-    #j = e.df_iticount['id_j'].values
-    #d = np.column_stack((i, j, distances_mine**2))
-    #my_dat = pd.DataFrame(d, columns=['id_i', 'id_j', 'dist'])
-    #my_dat.to_csv('dist_mine.csv', index=False)
-    distances_jhwi = pd.read_csv('distances_fetched_jhwi.csv')
-    np.testing.assert_array_almost_equal(distances_mine, 
+    distances_jhwi = pd.read_csv('./matlab_tests/data/distances_rand_dir.csv')
+    np.testing.assert_array_almost_equal(distances_mine,
                                          np.sqrt(distances_jhwi))
 
 
-def test_same_s_ij_model_directional():
+def test_same_fetched_distances_nondir():
+    e = estimate.Estimate('non_directional')
+    zeta = 2
+    #alpha has 26 entries, the num of cities.
+    alpha = np.ones(26)
+    lats = pd.read_csv('./matlab_tests/data/lats_rand_nondir.csv')
+    lats = lats.iloc[0, :].values
+    longs = pd.read_csv('./matlab_tests/data/longs_rand_nondir.csv')
+    longs = longs.iloc[0, :].values
+
+    distances_mine = e.fetch_dist(lats, longs).reshape((756,1))
+    distances_jhwi = pd.read_csv('./matlab_tests/data/distances_rand_nondir.csv')
+    np.testing.assert_array_almost_equal(distances_mine,
+                                         np.sqrt(distances_jhwi))
+
+
+def test_same_s_ij_model_directional_dir():
     e = estimate.Estimate('directional')
     zeta = 2
     #alpha has 26 entries, the num of cities.
     alpha = np.ones(26)
-    lats = pd.read_csv('lats_test.csv')
+    lats = pd.read_csv('./matlab_tests/data/lats_rand_dir.csv')
     lats = lats.iloc[0, :].values
-    longs = pd.read_csv('longs_test.csv')
+    longs = pd.read_csv('./matlab_tests/data/longs_rand_dir.csv')
     longs = longs.iloc[0, :].values
 
     dists = e.fetch_dist(lats, longs)
-    #df = pd.DataFrame(dists)
-    #df.to_csv('ll.csv', index=False)
 
     sij_mine = e.s_ij_model(zeta, alpha, dists).reshape((650, 1))
-    sij_jhwi = pd.read_csv('sij_jhwi.csv')
+    sij_jhwi = pd.read_csv('./matlab_tests/data/sij_rand_dir.csv')
     np.testing.assert_array_almost_equal(sij_mine, sij_jhwi)
 
 
-def test_same_sqerr_sum_directional():
+def test_same_s_ij_model_directional_nondir():
+    e = estimate.Estimate('non_directional')
+    zeta = 2
+    #alpha has 28 entries, the num of cities.
+    alpha = np.ones(28)
+    lats = pd.read_csv('./matlab_tests/data/lats_rand_nondir.csv')
+    lats = lats.iloc[0, :].values
+    longs = pd.read_csv('./matlab_tests/data/longs_rand_nondir.csv')
+    longs = longs.iloc[0, :].values
+
+    dists = e.fetch_dist(lats, longs)
+
+    sij_mine = e.s_ij_model(zeta, alpha, dists).reshape((756, 1))
+    sij_jhwi = pd.read_csv('./matlab_tests/data/sij_rand_nondir.csv')
+    np.testing.assert_array_almost_equal(sij_mine, sij_jhwi)
+
+
+def test_same_bounds_to_ipopt_dir_static():
+    e = estimate.Estimate('directional')
+    bounds = e.get_bounds(e.replace_id_coord(e.df_constr_stat), full_vars=True)
+
+    bounds_jhwi = pd.read_csv('./matlab_tests/data/bounds_dir_stat.csv')
+    bounds_jhwi = bounds_jhwi.replace(np.inf, 1.0e20)
+
+    np.testing.assert_array_almost_equal(np.array(bounds[0]),
+                                         bounds_jhwi['lb'].values.flatten())
+    np.testing.assert_array_almost_equal(np.array(bounds[1]),
+                                         bounds_jhwi['ub'].values.flatten())
+
+
+def test_same_bounds_to_ipopt_nondir_static():
+    e = estimate.Estimate('non_directional')
+    bounds = e.get_bounds(e.replace_id_coord(e.df_constr_stat), full_vars=True)
+
+    bounds_jhwi = pd.read_csv('./matlab_tests/data/bounds_nondir_stat.csv')
+    bounds_jhwi = bounds_jhwi.replace(np.inf, 1.0e20)
+
+    np.testing.assert_array_almost_equal(np.array(bounds[0]),
+                                         bounds_jhwi['lb'].values.flatten())
+    np.testing.assert_array_almost_equal(np.array(bounds[1]),
+                                         bounds_jhwi['ub'].values.flatten())
+
+
+def test_same_initial_condition_dir():
+    e = estimate.Estimate('directional')
+    theta0_mine = e.initial_cond(full_vars=True)
+    # zeta is one half sigma
+    theta0_mine[0] = theta0_mine[0]/2
+    theta0_jhwi = pd.read_csv('./matlab_tests/data/theta0_dir.csv')
+    np.testing.assert_array_almost_equal(theta0_mine, theta0_jhwi.values.flatten())
+
+
+def test_same_initial_condition_nondir():
+    e = estimate.Estimate('non_directional')
+    theta0_mine = e.initial_cond(full_vars=True)
+    # zeta is one half sigma
+    theta0_mine[0] = theta0_mine[0]/2
+    theta0_jhwi = pd.read_csv('./matlab_tests/data/theta0_nondir.csv')
+    np.testing.assert_array_almost_equal(theta0_mine, theta0_jhwi.values.flatten())
+
+
+def test_same_sqerr_sum_dir():
     e = estimate.Estimate('directional')
     zeta = [2]
     #alpha has 26 entries, the num of cities.
     alpha = np.ones(26).tolist()
-    lats = pd.read_csv('lats_test.csv')
-    longs = pd.read_csv('longs_test.csv')
-    
+    lats = pd.read_csv('./matlab_tests/data/lats_rand_dir.csv')
+    longs = pd.read_csv('./matlab_tests/data/longs_rand_dir.csv')
+
     sqerr_mine = np.ones((100, 1))
     for i in range(100):
         lat_guess = lats.iloc[i, :].tolist()
         lng_guess = longs.iloc[i, :].tolist()
-        print(lat_guess)
-        sqerr_mine[i] = e.sqerr_sum(zeta + alpha + lat_guess + lng_guess)
+        sqerr_mine[i] = e.sqerr_sum(zeta + lng_guess + lat_guess + alpha)
 
-    sqerr_jhwi = pd.read_csv('sqerr_jhwi.csv').values
+    sqerr_jhwi = pd.read_csv('./matlab_tests/data/sqerr_rand_dir.csv').values
     np.testing.assert_array_almost_equal(sqerr_mine, sqerr_jhwi)
+
+
+def test_same_sqerr_sum_nondir():
+    e = estimate.Estimate('non_directional')
+    zeta = [2]
+    #alpha has 28 entries, the num of cities.
+    alpha = np.ones(28).tolist()
+    lats = pd.read_csv('./matlab_tests/data/lats_rand_nondir.csv')
+    longs = pd.read_csv('./matlab_tests/data/longs_rand_nondir.csv')
+
+    sqerr_mine = np.ones((100, 1))
+    for i in range(100):
+        lat_guess = lats.iloc[i, :].tolist()
+        lng_guess = longs.iloc[i, :].tolist()
+        sqerr_mine[i] = e.sqerr_sum(zeta + lng_guess + lat_guess + alpha)
+
+    sqerr_jhwi = pd.read_csv('./matlab_tests/data/sqerr_rand_nondir.csv').values
+    np.testing.assert_array_almost_equal(sqerr_mine, sqerr_jhwi)
+
+
+def test_same_sqerr_sum_full_vars_dir():
+    e = estimate.Estimate('directional')
+    # Add useless param
+    zeta = [2, 2]
+    #alpha has 26 entries, the num of cities.
+    alpha = np.ones(26).tolist()
+    lats = pd.read_csv('./matlab_tests/data/lats_rand_dir.csv')
+    longs = pd.read_csv('./matlab_tests/data/longs_rand_dir.csv')
+
+    lats_known = e.df_known['lat_y'].values
+    lats_known = pd.DataFrame(np.tile(lats_known, (100, 1)))
+    lats = pd.concat([lats_known, lats], axis=1)
+
+    longs_known = e.df_known['long_x'].values
+    longs_known = pd.DataFrame(np.tile(longs_known, (100, 1)))
+    longs = pd.concat([longs_known, longs], axis=1)
+
+    sqerr_mine = np.ones((100, 1))
+    for i in range(100):
+        lat_guess = lats.iloc[i, :].tolist()
+        lng_guess = longs.iloc[i, :].tolist()
+        sqerr_mine[i] = e.sqerr_sum(zeta + lng_guess + lat_guess + alpha,
+                                   full_vars=True)
+
+    sqerr_jhwi = pd.read_csv('./matlab_tests/data/sqerr_rand_dir.csv').values
+    np.testing.assert_array_almost_equal(sqerr_mine, sqerr_jhwi)
+
+
+def test_same_sqerr_sum_full_vars_nondir():
+    e = estimate.Estimate('non_directional')
+    # Add useless param
+    zeta = [2, 2]
+    #alpha has 26 entries, the num of cities.
+    alpha = np.ones(28).tolist()
+    lats = pd.read_csv('./matlab_tests/data/lats_rand_nondir.csv')
+    longs = pd.read_csv('./matlab_tests/data/longs_rand_nondir.csv')
+
+    lats_known = e.df_known['lat_y'].values
+    lats_known = pd.DataFrame(np.tile(lats_known, (100, 1)))
+    lats = pd.concat([lats_known, lats], axis=1)
+
+    longs_known = e.df_known['long_x'].values
+    longs_known = pd.DataFrame(np.tile(longs_known, (100, 1)))
+    longs = pd.concat([longs_known, longs], axis=1)
+
+    sqerr_mine = np.ones((100, 1))
+    for i in range(100):
+        lat_guess = lats.iloc[i, :].tolist()
+        lng_guess = longs.iloc[i, :].tolist()
+        sqerr_mine[i] = e.sqerr_sum(zeta + lng_guess + lat_guess + alpha,
+                                   full_vars=True)
+
+    sqerr_jhwi = pd.read_csv('./matlab_tests/data/sqerr_rand_nondir.csv').values
+    np.testing.assert_array_almost_equal(sqerr_mine, sqerr_jhwi)
+
+
+def test_same_gradients_at_initial_coords_dir():
+    e = estimate.Estimate('directional')
+    varlist = e.initial_cond(full_vars=True)
+    grad_mine = e.grad_full_vars(varlist)
+
+    grad_jhwi = pd.read_csv('./matlab_tests/data/gradient0_dir.csv')
+    grad_jhwi = grad_jhwi.values.flatten()
+
+    # sigma is one half zeta
+    grad_jhwi[0] = grad_jhwi[0]/2
+
+    np.testing.assert_array_almost_equal(grad_mine, grad_jhwi)
+
+
+def test_same_gradients_at_initial_coords_nondir():
+    e = estimate.Estimate('non_directional')
+    varlist = e.initial_cond(full_vars=True)
+    grad_mine = e.grad_full_vars(varlist)
+
+    grad_jhwi = pd.read_csv('./matlab_tests/data/gradient0_nondir.csv')
+    grad_jhwi = grad_jhwi.values.flatten()
+
+    # sigma is one half zeta
+    grad_jhwi[0] = grad_jhwi[0]/2
+
+    np.testing.assert_array_almost_equal(grad_mine, grad_jhwi)
+
+
+def test_many_inputs_grad_and_objective_dir():
+    e = estimate.Estimate('directional')
+    inputs = pd.read_csv('./matlab_tests/data/inputs_dir.csv')
+    # Sigma to zeta
+    inputs.iloc[:, 0] = inputs.iloc[:, 0]*2
+    grads_mine = np.empty((1000, 80))
+    obj_mine = np.empty((1000, 1))
+    for i in range(len(inputs)):
+        inp = inputs.iloc[i, :].values
+        obj_mine[i] = e.sqerr_sum(inp, full_vars = True)
+        grads_mine[i, :] = e.grad_full_vars(inp)
+
+    obj_jhwi = pd.read_csv('./matlab_tests/data/inputs_objective_dir.csv').values
+    grads_jhwi = pd.read_csv('./matlab_tests/data/inputs_gradients_dir.csv').values
+    grads_jhwi[:, 0] = grads_jhwi[:, 0]/2
+    np.testing.assert_array_almost_equal(obj_mine, obj_jhwi)
+    np.testing.assert_array_almost_equal(grads_mine, grads_jhwi)
+
+
+def test_many_inputs_grad_and_objective_nondir():
+    e = estimate.Estimate('non_directional')
+    inputs = pd.read_csv('./matlab_tests/data/inputs_nondir.csv')
+    # Sigma to zeta
+    inputs.iloc[:, 0] = inputs.iloc[:, 0]*2
+    grads_mine = np.empty((1000, 86))
+    obj_mine = np.empty((1000, 1))
+    for i in range(len(inputs)):
+        inp = inputs.iloc[i, :].values
+        obj_mine[i] = e.sqerr_sum(inp, full_vars = True)
+        grads_mine[i, :] = e.grad_full_vars(inp)
+
+    obj_jhwi = pd.read_csv('./matlab_tests/data/inputs_objective_nondir.csv').values
+    grads_jhwi = pd.read_csv('./matlab_tests/data/inputs_gradients_nondir.csv').values
+    grads_jhwi[:, 0] = grads_jhwi[:, 0]/2
+    np.testing.assert_array_almost_equal(obj_mine, obj_jhwi)
+    np.testing.assert_array_almost_equal(grads_mine, grads_jhwi)
+
+
+
+
+def gen_test_inputs(l=1000, full_vars=False, directional=True):
+    if directional:
+        n_cities = 26
+        name = 'inputs_dir'
+    else:
+        n_cities = 28
+        name = 'inputs_nondir'
+
+    alphas = np.random.uniform(0, 50, (l, n_cities))
+    zeta = np.random.uniform(0, 10, l)
+    if full_vars:
+        lngs = np.random.uniform(27, 45, (l, n_cities))
+        lats = np.random.uniform(36, 42, (l, n_cities))
+        df = pd.DataFrame( np.column_stack((zeta,
+                                            2*np.ones(l),
+                                            lngs,
+                                            lats,
+                                            alphas)))
+    df.to_csv("~/" + name + ".csv", index=False)